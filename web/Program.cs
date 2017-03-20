1621b8c7fea14726e4bb358cf2fa111d0113f803
supermitsuba <supermitsuba@supermitsubas-MacBook-Air.local>
2017-03-20 18:58:50 -0400

    added listen on all network interfaces

--- web/Program.cs
+++ web/Program.cs
@@ -10,6 +10,7 @@ namespace WebApplication
             var host = new WebHostBuilder()
                 .UseKestrel()
                 .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseUrls("http://*:5000")
                 .UseIISIntegration()
                 .UseStartup<Startup>()
                 .Build();
