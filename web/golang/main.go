ff3339e622bf69a9629910108a4eb3b5dd8d99e0
supermitsuba <supermitsuba@supermitsubas-MacBook-Air.local>
2017-03-27 05:51:27 -0400

    Created web service go

---
+++ web/golang/main.go
@@ -0,0 +1,116 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+
+	"github.com/gorilla/mux"
+	"github.com/streadway/amqp"
+)
+
+type Message struct {
+	Duration int    `json:"duration"` // 1 to 60 validation
+	Text     string `json:"text"`     //32 max validation
+}
+
+func main() {
+
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/api/test", Index)
+	router.HandleFunc("/api/message", MessageFunc)
+	log.Fatal(http.ListenAndServe(":5000", router))
+}
+
+func Index(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Working...")
+}
+
+func MessageFunc(w http.ResponseWriter, r *http.Request) {
+	var priority = r.URL.Query().Get("priority")
+	var item = new(Message)
+	json.NewDecoder(r.Body).Decode(item)
+
+	if priority == "" {
+		priority = "false"
+	}
+
+	var isBool, err = strconv.ParseBool(priority)
+	if err != nil {
+		http.Error(w, "Invalid priority", 400)
+		return
+	}
+
+	if item.Duration < 1 || item.Duration > 60 {
+		http.Error(w, "Invalid duration", 400)
+		return
+	}
+
+	if len(item.Text) > 32 {
+		http.Error(w, "Invalid text", 400)
+		return
+	}
+
+	if isBool {
+		str, err := json.Marshal(item)
+		if err != nil {
+			http.Error(w, "Invalid item", 400)
+			return
+		}
+		SendMessage(os.Args[1], "priority", str)
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK Priority"))
+	} else {
+		str, err := json.Marshal(item)
+		if err != nil {
+			http.Error(w, "Invalid item", 400)
+			return
+		}
+		SendMessage(os.Args[1], "messages", str)
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	}
+
+	return
+}
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Fatalf("%s: %s", msg, err)
+	}
+}
+
+func SendMessage(amqpUrl string, queueName string, body []byte) {
+	conn, err := amqp.Dial(amqpUrl)
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		queueName, // name
+		true,      // durable
+		false,     // delete when unused
+		false,     // exclusive
+		false,     // no-wait
+		nil,       // arguments
+	)
+	failOnError(err, "Failed to declare a queue")
+
+	err = ch.Publish(
+		"",     // exchange
+		q.Name, // routing key
+		false,  // mandatory
+		false,  // immediate
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        body,
+		})
+	log.Printf(" [x] Sent %s", body)
+	failOnError(err, "Failed to publish a message")
+}
