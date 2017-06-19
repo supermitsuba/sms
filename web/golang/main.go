9a457a384fd3c58fe2f62f6019090c2674b7567d
supermitsuba <supermitsuba@supermitsubas-MacBook-Air.local>
2017-06-18 21:21:48 -0400

    Added weather api call

--- web/golang/main.go
+++ web/golang/main.go
@@ -8,6 +8,8 @@ import (
 	"net/http"
 	"os"
 	"strconv"
+	"strings"
+	"time"
 
 	"github.com/gorilla/mux"
 	"github.com/streadway/amqp"
@@ -21,9 +23,11 @@ type Message struct {
 func main() {
 
 	router := mux.NewRouter().StrictSlash(true)
-	router.HandleFunc("/", Index)
-	router.HandleFunc("/api/test", Test)
-	router.HandleFunc("/api/message", MessageFunc)
+	router.HandleFunc("/", Index).Methods("GET")
+	router.HandleFunc("/api/test", Test).Methods("GET")
+	router.HandleFunc("/api/message", MessageFunc).Methods("POST")
+	router.HandleFunc("/api/weather", WeatherFunc).Methods("POST")
+	router.HandleFunc("/api/forecast", ForecastFunc).Methods("POST")
 	log.Fatal(http.ListenAndServe(":5000", router))
 }
 
@@ -86,6 +90,58 @@ func MessageFunc(w http.ResponseWriter, r *http.Request) {
 	return
 }
 
+func WeatherFunc(w http.ResponseWriter, r *http.Request) {
+	weather := GetWeather(os.Args[2])
+	temp1 := (weather.Main.Temperature * 9 / 5) - 459.17
+	temp := strconv.FormatFloat(temp1, 'f', 0, 64)
+	var item = new(Message)
+	item.Text = "Now:     " + temp + " F   " + weather.WeatherSections[0].Main
+	item.Duration = 15
+
+	str, err := json.Marshal(item)
+	if err != nil {
+		http.Error(w, "Invalid item", 400)
+		return
+	}
+	SendMessage(os.Args[1], "messages", str)
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
+
+	return
+}
+
+func ForecastFunc(w http.ResponseWriter, r *http.Request) {
+	weather := GetForecast(os.Args[3])
+
+	for i := 0; i < len(weather.WeatherForcast); i++ {
+		item := weather.WeatherForcast[i]
+
+		splitDateTime := strings.Split(item.DateOfTemperature, " ")
+		splitHour := strings.Split(splitDateTime[1], ":")[0]
+		if splitHour == "15" {
+			t1, err := time.Parse("2006-01-02", splitDateTime[0])
+			failOnError(err, "Could not parse datetime properly.")
+			dayOfTheWeek := t1.Format("Mon")
+			temp := (item.Main.Temperature * 9 / 5) - 459.17
+			temp1 := strconv.FormatFloat(temp, 'f', 0, 64)
+			var myMessage = new(Message)
+			myMessage.Text = dayOfTheWeek + ":       " + temp1 + " F " + item.WeatherSections[0].Main
+			myMessage.Duration = 15
+
+			str, err := json.Marshal(myMessage)
+			if err != nil {
+				http.Error(w, "Invalid item", 400)
+				return
+			}
+			SendMessage(os.Args[1], "messages", str)
+		}
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
+	return
+}
+
 func failOnError(err error, msg string) {
 	if err != nil {
 		log.Fatalf("%s: %s", msg, err)
@@ -122,4 +178,60 @@ func SendMessage(amqpUrl string, queueName string, body []byte) {
 		})
 	log.Printf(" [x] Sent %s", body)
 	failOnError(err, "Failed to publish a message")
+}
+
+func GetWeather(URL string) WeatherModel {
+	res, err := http.Get(URL)
+	if err != nil {
+		panic(err.Error())
+	}
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	var s = new(WeatherModel)
+	err1 := json.Unmarshal(body, &s)
+	if err1 != nil {
+		fmt.Println("whoops:", err1)
+	}
+
+	return *s
+}
+
+func GetForecast(URL string) ForecastModel {
+	res, err := http.Get(URL)
+	if err != nil {
+		panic(err.Error())
+	}
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	var s = new(ForecastModel)
+	err1 := json.Unmarshal(body, &s)
+	if err1 != nil {
+		fmt.Println("whoops:", err1)
+	}
+
+	return *s
+}
+
+type WeatherModel struct {
+	WeatherSections   []WeatherSection `json:"weather"`
+	Main              MainSection      `json:"main"`
+	DateOfTemperature string           `json:"dt_txt"`
+}
+
+type WeatherSection struct {
+	Main string `json:"main"`
+}
+
+type MainSection struct {
+	Temperature float64 `json:"temp"`
+}
+
+type ForecastModel struct {
+	WeatherForcast []WeatherModel `json:"list"`
 }
