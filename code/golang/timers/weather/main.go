5a5293a7cc21f984617ae34434f6480470a741a1
Jorden Lowe <supermitsuba@users.noreply.github.com>
2019-01-20 21:32:45 -0500

    Update main.go
    conversion issue

--- code/golang/timers/weather/main.go
+++ code/golang/timers/weather/main.go
@@ -15,7 +15,7 @@ import (
 func main() {
 	weather := GetWeather(os.Args[2])
 	// temp1 := (weather.Main.Temperature * 9 / 5) - 459.17
-	temp := weather.Main.Temperature // strconv.FormatFloat(temp1, 'f', 0, 64)
+	temp := strconv.FormatFloat(weather.Main.Temperature, 'f', 0, 64)
 	message := Message{
 		Text:     "Now:     " + temp + " F   " + weather.WeatherSections[0].Main,
 		Duration: 30,
